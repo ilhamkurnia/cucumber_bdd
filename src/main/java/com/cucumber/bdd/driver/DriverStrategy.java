234197eea5883f1490afbac4b776dea1db6325db
ilham kurnia <ilhammcd@gmail.com>
2022-06-18 14:38:52 +0700

    create driver & utils

---
+++ src/main/java/com/cucumber/bdd/driver/DriverStrategy.java
@@ -0,0 +1,9 @@
+package com.cucumber.bdd.driver;
+
+import org.openqa.selenium.WebDriver;
+
+public interface DriverStrategy {
+
+	public WebDriver setStrategy();
+	
+}
---
+++ src/main/java/com/cucumber/bdd/driver/DriverStrategyImplementator.java
@@ -0,0 +1,22 @@
+package com.cucumber.bdd.driver;
+
+import com.cucumber.bdd.utils.Constants;
+
+public class DriverStrategyImplementator {
+
+	public static DriverStrategy chooseStrategy(String strategy) {
+		
+		switch(strategy) {
+		case Constants.CHROME:
+			return new Chrome();
+			
+		case Constants.FIREFOX:
+			return new Firefox();
+		
+		default:
+			return null;
+		}
+		
+	}
+	
+}
